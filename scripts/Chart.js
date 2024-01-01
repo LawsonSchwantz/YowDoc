46a8bdf9defd35a0f56a7c387d2e875a3d9432b8
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2024-01-01 21:54:51 +0700

    Add files via upload

---
+++ scripts/Chart.js
@@ -0,0 +1,11 @@
+document.getElementById("payment").addEventListener("click", function() {
+    window.location.href = "payment.html";
+});
+
+document.getElementById("payment2").addEventListener("click", function() {
+    window.location.href = "payment.html";
+});
+
+document.getElementById("payment3").addEventListener("click", function() {
+    window.location.href = "payment.html";
+});
